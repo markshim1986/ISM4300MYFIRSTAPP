1857ca122664c63ea6d8ff2a4905a12c32e50da9
markshim1986 <mshim@mail.usf.eud>
2019-08-29 16:30:17 -0400

    Add project files.

---
+++ ISM4300MYFIRSTAPP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ISM4300MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Write line to print to screen
+            Console.WriteLine("This is my first Console app");
+
+            // Write line to print to screen
+            Console.WriteLine("I am learning to publish Github");
+
+
+
+        }
+    }
+}
